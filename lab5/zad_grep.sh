608a1a48d259404c6ddf94b9dc34a6552531a5e9
irezwi <zwirek.ireneusz@gmail.com>
2019-05-14 03:06:23 +0200

    Hotfix lab 5

--- lab5/zad_grep.sh
+++ lab5/zad_grep.sh
@@ -1,10 +1,8 @@
-contain_todo=0
-
 grep -r "TODO" $1 | wc -l
 grep -ch -r -e "^$" $1
 grep -ch -r -v -e "^$" $1
 grep -ch -r -e ";$" $1
-grep -r -e "{\|}" $1 | grep -v "{.*}\|{.*}" | wc -l
+grep -r -e "{\|}" $1 | grep -v "{.*}\|}.*{" | wc -l
 grep -ch -r -v -e "[A-Za-z]" $1
 grep -ch -r -e "^.*[(]{2}.*$" $1
 grep -ch -r -e "0x[A-Fa-f0-9]" $1
