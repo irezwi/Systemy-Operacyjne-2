f11cccc9092974af4f0c371a19b443c139291a14
irezwi <zwirek.ireneusz@gmail.com>
2019-05-27 20:47:35 +0200

    Exercise 8 simplified

--- lab6/zad8.py
+++ lab6/zad8.py
@@ -2,6 +2,9 @@ import sys
 import os
 
 path = os.path.abspath(sys.argv[1])
-for root, dirs, files in os.walk(path):
-    for d in dirs:
-        os.rename(os.path.join(root, d), os.path.join(root, d) + '-copy')
+for d in os.listdir(path):
+    d = os.path.join(path, d)
+    if os.path.isdir(d):
+        os.rename(d, d + '-copy')
+
+
